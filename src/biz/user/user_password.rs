304b9896d193dc1176b7dcd8f74af28f99d618f0
metacodebean <caokun8008@gmail.com>
2025-10-22 21:25:45 +0800

    服务端实现设置密码，密码登录

---
+++ src/biz/user/user_password.rs
@@ -0,0 +1,72 @@
+use app_error::AppError;
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+use unicode_segmentation::UnicodeSegmentation;
+
+#[derive(Debug)]
+pub struct UserPassword(pub String);
+
+impl UserPassword {
+  pub fn parse(s: String) -> Result<UserPassword, AppError> {
+    if s.trim().is_empty() {
+      return Err(AppError::InvalidRequest("Password is empty".to_string()));
+    }
+
+    if s.graphemes(true).count() > 100 {
+      return Err(AppError::InvalidRequest(
+        "Password is too long".to_string(),
+      ));
+    }
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+    if contains_forbidden_characters {
+      return Err(AppError::InvalidRequest(
+        "Password contains forbidden characters".to_string(),
+      ));
+    }
+
+    if !validate_password(&s) {
+      return Err(AppError::InvalidRequest(
+        "Password format is invalid".to_string(),
+      ));
+    }
+
+    Ok(Self(s))
+  }
+}
+
+impl AsRef<str> for UserPassword {
+  fn as_ref(&self) -> &str {
+    &self.0
+  }
+}
+
+lazy_static! {
+    // Test it in https://regex101.com/
+    // https://stackoverflow.com/questions/2370015/regular-expression-for-password-validation/2370045
+    // Hell1!
+    // [invalid, greater or equal to 6]
+    // Hel1!
+    //
+    // Hello1!
+    // [invalid, must include number]
+    // Hello!
+    //
+    // Hello12!
+    // [invalid must include upper case]
+    // hello12!
+    static ref PASSWORD: Regex = Regex::new("((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\\W]).{6,20})").unwrap();
+}
+
+pub fn validate_password(password: &str) -> bool {
+  match PASSWORD.is_match(password) {
+    Ok(is_match) => is_match,
+    Err(e) => {
+      tracing::error!("validate_password fail: {:?}", e);
+      false
+    },
+  }
+}
+
+
